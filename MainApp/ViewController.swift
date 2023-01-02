06679bcda57667dc3cfab846af938984a15db85b
Amit Jangirh <amitjangirh@gmail.com>
2023-01-02 17:32:36 +0530

    Initial Commit

---
+++ MainApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainApp
+//
+//  Created by amit jangirh on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
